03d55d0ad255f14b3a5dfebc3a2f3897ae517164
7PXS <godx00@outlook.com>
2025-05-29 21:52:31 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
-export const metadata: Metadata = {
-  title: "Sigma Site",
-  description: "Goon.Cool",
+export const metadata = {
+  title: 'Sigma Site',
+  description: 'Goon.Cool',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
