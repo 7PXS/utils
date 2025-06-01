0f5f80a0f00412a64bba12c25ae8ee5d9db91d1f
7PXS <godx00@outlook.com>
2025-06-01 00:48:56 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { GeistSans, GeistMono } from '@next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
+const geistSans = GeistSans({
   variable: '--font-geist-sans',
   subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
 });
@@ -19,9 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
     </html>
