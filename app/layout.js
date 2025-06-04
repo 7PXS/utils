d05983f9a74760add83eb1094621b76c3f712b9c
7PXS <godx00@outlook.com>
2025-06-03 20:22:30 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,9 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="icon" href="/app/favicon.ico" />
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         {children}
       </body>
     </html>
