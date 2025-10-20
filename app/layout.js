a2077d8e0f91ee2b471b5e5a07b207d4e5383db7
7PXS <godx00@outlook.com>
2025-10-20 16:21:19 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,8 @@
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Inter } from 'next/font/google';
 import './globals.css';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const inter = Inter({
+  variable: '--font-inter',
   subsets: ['latin'],
 });
 
@@ -22,7 +17,7 @@ export default function RootLayout({ children }) {
       <head>
         <link rel="icon" href="https://cdn.discordapp.com/avatars/155149108183695360/b4fdfc64edff74c37e1574d34fad66c2.webp?size=1024" />
       </head>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+      <body className={`${inter.variable} antialiased`} >
         {children}
       </body>
     </html>
